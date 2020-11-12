b341ca73dc20cdd5731ad74eab102db03eaa352a
David Gleba <dgleba@gmail.com>
2020-11-12 08:33:55 -0500

    misc

--- cisi543b/apprails/app/models/ability.rb
+++ cisi543b/apprails/app/models/ability.rb
@@ -1,17 +1,16 @@
 class Ability
   include CanCan::Ability
 
+  # remember to add roles to user.rb
+  # http://hibbard.eu/authentication-with-devise-and-cancancan-in-rails-4-2/  
+  
+
   def initialize(user)
 
     if user.lr_admin?
       can :manage, :all
       can :access, :rails_admin       # only allow admin users to access Rails Admin
       can :dashboard,  :all                # allow access to dashboard
-
-    # remember to add roles to user.rb
-    #
-    # http://hibbard.eu/authentication-with-devise-and-cancancan-in-rails-4-2/  
-    #
 
     elsif user.lr_regular?
       can :read, [  Role,    Benefit,  ]
@@ -35,7 +34,7 @@ class Ability
     
     elsif user.lr_readonly?
       can :read, [  Role,  ]
-      can :dashboard, :all
+      #can :dashboard, :all
     end
     
     # Feasibility, WasteType,
