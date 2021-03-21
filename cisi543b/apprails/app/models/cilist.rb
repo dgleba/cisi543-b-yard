4ee22eecc3fd42a2e548a5591659880f7f38ecd5
David Gleba <dgleba@gmail.com>
2021-03-20 20:10:21 -0400

    add.contact.to.cilist-view

--- cisi543b/apprails/app/models/cilist.rb
+++ cisi543b/apprails/app/models/cilist.rb
@@ -3,7 +3,13 @@ class Cilist < ApplicationRecord
   belongs_to :benefit
   belongs_to :implementation_status
   # belongs_to :feasibility
+
+
+ 
+  # 2021-03-07_Sun_13.49-PM
   belongs_to :user
+  # belongs_to :user, :class_name => "User"
+  # belongs_to :responsible_person, :class_name => "User"  
 
   has_paper_trail
   
